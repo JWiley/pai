584d64c75ffe4344723687fa7c38c808d4895af2
Joshua Wiley <jwiley.psych@gmail.com>
2014-11-20 12:46:35 +1100

    minor updates implementing 4df array for results

--- R/cvboot.R
+++ R/cvboot.R
@@ -34,7 +34,6 @@ indexer <- function(i, end) {
 #'   Must be constructed manually.
 #' @param yhigherisbetter A logical value whether higher scores indicate better
 #'   treatment response on the dependent variable
-#' @param seed A seed to make the bootstrap reproducible
 #' @return A three dimensional array with cases on the first dimension,
 #'   multiple estimates calculated on the second dimension, and bootstrap resamples
 #'   on the third dimension.
@@ -56,11 +55,11 @@ indexer <- function(i, end) {
 #' # remove names
 #' names(holdouts) <- NULL
 #'
-#' m <- lm_pai_cvboot(formula, DV, TreatVar, dat, boot = TRUE, k = k, holdouts = holdouts)
+#' m <- lm_pai_cvboot_fit(formula, DV, TreatVar, dat, boot = TRUE, k = k, holdouts = holdouts)
 #'
 #' # see result from first bootstrap resample
 #' m[, , 1]
-lm_pai_cvboot <- function(formula, DV, TreatVar, dat, boot = TRUE, k, holdouts, yhigherisbetter = TRUE, seed = 1234) {
+lm_pai_cvboot_fit <- function(formula, DV, TreatVar, dat, boot = TRUE, k, holdouts, yhigherisbetter = TRUE) {
 
   stopifnot(all(unique(dat[, TreatVar]) %in% c(0, 1)))
 
@@ -69,7 +68,6 @@ lm_pai_cvboot <- function(formula, DV, TreatVar, dat, boot = TRUE, k, holdouts, 
     ntrain <- nrow(dat) - nholdout
 
     if (boot) {
-      set.seed(seed)
       boot.index <- sample(1:ntrain, size = ntrain * k, replace = TRUE)
     } else {
       boot.index <- 1:ntrain
@@ -93,6 +91,7 @@ lm_pai_cvboot <- function(formula, DV, TreatVar, dat, boot = TRUE, k, holdouts, 
                    Yhat.reality = yhat[(2 * nholdout + 1):(3 * nholdout)],
                    Yhat.0 = yhat[1:nholdout],
                    Yhat.1 = yhat[(nholdout + 1):(2 * nholdout)])
+      colnames(out)[2] <- TreatVar
       out <- cbind(out,
                    Yhat.PAI = out[, "Yhat.0"] - out[, "Yhat.1"],
                    Residual = out[, "Y"] - out[, "Yhat.reality"])
@@ -131,53 +130,87 @@ lm_pai_cvboot <- function(formula, DV, TreatVar, dat, boot = TRUE, k, holdouts, 
 #' @param DV a character string indicating the name of the dependent variable
 #' @param TreatVar a character string indicating the name of the treatment variable
 #'   (note that values must be coded as 0/1 for the treatment variable.
-#' @param imputedData An imputed dataset (list of datasets) to use
-#' @param boot A logical value whether to bootstrap or not
-#' @param k The number of bootstrap resamples to take
-#' @param holdouts The holdouts to be used.  If a vector from 1 to the number of
-#'   cases available in the dataset, it is equivalent to leave-one-out.
-#'   Must be constructed manually.
-#' @param yhigherisbetter A logical value whether higher scores indicate better
-#'   treatment response on the dependent variable
-#' @param seed A seed to make the bootstrap reproducible
+#' @param data A list of imputed datasets or for a single dataset, a list of one
+#' @param nboot The number of bootstrap resamples to take
+#' @param holdouts The holdouts to be used.  This should be a numeric list with as many elements
+#'   desired for holdouts.  For example, if the list is the same length
+#'   as the number of cases and each element is one case (i.e., 1, 2, \ldots, N),
+#'   then it is equivalent to leave-one-out.  If there are ten elements, each containing
+#'   approximately 1/10th of the sample, it is 10-fold cross validation, etc.
+#'   If a single character string is passed, K-fold CV is assumed and holdouts
+#'   are automatically constructed.
 #' @param cl An existing cluster to use (optional)
 #' @param cores The number of cores to use when creating a cluster, if an existing
 #'   cluster is not passed (optional).  If left blank and no cluster passed,
 #'   defaults to the number of cores available.
-#' @return A three dimensional array with cases on the first dimension,
-#'   multiple estimates calculated on the second dimension, and bootstrap resamples
-#'   on the third dimension.
+#' @param seed A seed to make the bootstrap reproducible
+#' @param boot A logical value whether to bootstrap or not
+#' @param yhigherisbetter A logical value whether higher scores indicate better
+#'   treatment response on the dependent variable
+#' @return A list containing a four dimensional array of the results, as well as
+#'   the input parameters for the model.  For the array, the first dimension is cases,
+#'   the second dimension is multiple estimates calculated, the third dimension is
+#'   bootstrap resamples, and the fourth dimension is imputations.
 #' @export
-#' @import doParallel doRNG mice
+#' @import mice
 #' @examples
-#' # make me!!!
-mi_lm_pai_cvboot <- function(formula, DV, TreatVar, imputedData, boot = TRUE, k = 500, holdouts, yhigherisbetter = TRUE, seed = 1234, cl, cores) {
+#' # builtin dataset with 32 cases
+#' dat <- mtcars
+#' dat$cyl <- factor(dat$cyl)
+#' formula <- ~ cyl + am * (mpg + hp + drat + wt)
+#' DV <- "disp"
+#' TreatVar <- "am"
+#' k <- 50 # use a few just for speed
+#' holdouts <- 1:32 # for leave-one-out
+#'
+#' m <- mi_lm_pai_cvboot(formula, DV, TreatVar, list(dat, dat), nboot = k, holdouts = holdouts)
+#'
+#' # see result from first bootstrap resample
+mi_lm_pai_cvboot <- function(formula, DV, TreatVar, data, nboot = 500, holdouts,
+                             cl, cores, seed = 1234, boot = TRUE, yhigherisbetter = TRUE) {
+
+  N <- length(data)
+  ncases <- nrow(data[[1]])
+
+  if (length(holdouts) == 1 && is.character(holdouts)) {
+    set.seed(seed)
+    holdouts <- by(sample(1:ncases), cut(1:ncases,
+      ceiling(seq(from = 0, to = ncases, length.out = as.numeric(holdouts) + 1))),
+      function(x) x)
+  }
+
   if (missing(cl)) {
     if (missing(cores)) {
       cores <- detectCores()
     }
     cl <- makeCluster(cores)
+    on.exit(stopCluster(cl))
   }
-
-  on.exit(stopCluster(cl))
 
   clusterEvalQ(cl, {
     library(pai)
   })
 
-  Nimp <- length(imputedData)
-
-  clusterExport(cl, c("indexer", "lm_pai_cvboot", "holdouts"))
-
   env <- environment()
-  clusterExport(cl, c("imputedData"), envir = env)
+  clusterExport(cl, c("formula", "DV", "TreatVar", "data", "nboot", "holdouts", "yhigherisbetter"),
+    envir = env)
 
-  set.seed(seed)
+  clusterSetRNGStream(cl, seed)
 
-  parLapplyLB(cl, 1:Nimp, function(i) {
-    lm_pai_cvboot(formula = formula,
-      DV = DV, TreatVar = TreatVar, dat = imputedData[[i]],
-      boot = boot, k = k, holdouts = holdouts,
-      yhigherisbetter = yhigherisbetter, seed = seed)
+  out <- parLapply(cl, 1:N, function(i) {
+    lm_pai_cvboot_fit(formula = formula,
+      DV = DV, TreatVar = TreatVar, dat = data[[i]],
+      boot = boot, k = nboot, holdouts = holdouts,
+      yhigherisbetter = yhigherisbetter)
   })
+
+  finalout <- array(NA_real_, c(dim(out[[1]]), N),
+    dimnames = list(NULL, colnames(out[[1]]), NULL, NULL))
+
+  for (i in 1:N) {
+    finalout[, , , i] <- out[[i]]
+  }
+
+  list(results = finalout, DV = DV, TreatVar = TreatVar, nboot = nboot,
+       holdouts = holdouts, seed = seed, yhigherisbetter = yhigherisbetter)
 }
